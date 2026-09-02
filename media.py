1a0bd6a29d861c9923f015d30a97ee8f0ce6a24d
Laryssa Mota S. <motalaryssa173@gmail.com>
2026-09-02 17:16:45 -0300

    Add files via upload

---
+++ media.py
@@ -0,0 +1,16 @@
+nota1 = float(input("Digite a 1ª nota: "))
+nota2 = float(input("Digite a 2ª nota: "))
+nota3 = float(input("Digite a 3ª nota: "))
+nota4 = float(input("Digite a 4ª nota: "))
+
+media = (nota1 + nota2 + nota3 + nota4) / 4
+
+if media <5.0: 
+    print(f"Sua nota: {media} | Reprovado(a)")
+
+elif  (media >= 5.0) and (media <7.0):
+    print(f"Sua nota: {media} | Recuperação")
+
+else:
+    print(f"Sua nota: {media} | Aprovado(a)")
+
